8de3ca97faaf1427efbe40b87848f38d63057be7
折田幸夫 <3204704@gmail.com>
2021-03-28 18:59:52 +0900

    add comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Card from "../components/Card";
 import Layout from "../components/Layout";
 import { cardList } from "../utils/card-list";
 
+// TODO カードを 4 * 13 で並べる
+// TODO カードの並び順を random() を用いて並び替える。(配列いじるか？)
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>神経衰弱 at Next.js 👋</h1>
