d769299898d5d35dc93f40cbb8fa2704c5271dec
折田幸夫 <3204704@gmail.com>
2021-04-21 00:57:09 +0900

    feat styling at styled-components/apply Global Style

---
+++ src/pages/_app.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import App, { Container } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { reset } from "styled-reset";
+
+const fontFamily =
+  "arial, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Hiragino Sans, sans-serif";
+
+export const createMyGlobalStyle = ({
+  fontFamily,
+}: {
+  fontFamily: string;
+}): any => createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    /* stylelint-disable-next-line declaration-property-unit-allowed-list */
+    font-size: 62.5%;
+    /* Headerの高さ */
+    scroll-padding-top: 56px;
+  }
+  body {
+    color: #f3efef;
+    font-weight: Bold;
+    /* stylelint-disable-next-line declaration-property-unit-allowed-list */
+    font-size: 1.4em;
+    font-family: ${fontFamily};
+    -webkit-font-smoothing: antialiased;
+    background-color:#343440;
+  }
+  h1 {
+    color:white;
+  }
+  a {
+    text-decoration: none;
+  }
+  img {
+    vertical-align: middle;
+  }
+  button {
+    padding: 0;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    appearance: none;
+  }
+  input {
+    font-size: inherit;
+    border: none;
+    outline: 0;
+    /* IEとEdgeのデフォルトの×ボタンを非表示 */
+    &::-ms-clear {
+      display: none;
+    }
+  }
+  
+`;
+const GlobalStyle = createMyGlobalStyle({ fontFamily });
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    );
+  }
+}
