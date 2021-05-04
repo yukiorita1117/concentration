c7770eecab5a6ce407baa9059b6380868d36affe
折田幸夫 <3204704@gmail.com>
2021-05-05 01:03:58 +0900

    fix bug and remove unused comment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import styled from "styled-components";
-import ReverceImage from "./ReverceImage";
 
 type Props = {
   mark: string;
   num: number;
 };
-
-const StyledReverceImage = styled(ReverceImage)``;
 
 const Container = styled.div`
   width: 50px;
@@ -20,67 +17,9 @@ const Container = styled.div`
   background-color: white;
 `;
 
-const stockCardNumber = [];
-
-const compareNum = (numArray: number[]) => {
-  // compare cards. if match => match flag is true.
-  if (numArray[0] === numArray[1]) {
-    console.log(
-      "マッチ!!!!🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉",
-      numArray[0],
-      numArray[1]
-    );
-    return true;
-  }
-  // もし false だった場合（カードがマッチしなかった場合） は isdisable をtrue にする必要がある。
-  if (numArray[0] !== numArray[1]) {
-    return false;
-  }
-};
-
 const Card = ({ mark, num }: Props) => {
-  const [isdisable1, setIsdisable1] = React.useState(true);
-  const [isdisable2, setIsdisable2] = React.useState(true);
-
-  const handleClick = (e: any) => {
-    console.log("クリックイベント::番号", num);
-
-    if (stockCardNumber.length === 0) {
-      console.log("あああああ");
-      setIsdisable1(false);
-      stockCardNumber.push(num);
-    } else if (stockCardNumber.length === 1) {
-      console.log("ええええ");
-
-      setIsdisable2(false);
-      stockCardNumber.push(num);
-
-      const flag = compareNum(stockCardNumber);
-      if (flag === false) {
-        setTimeout(() => {
-          // カードを裏側にする。
-          setIsdisable1(true);
-          setIsdisable2(true);
-        }, 1500);
-        console.log("1,2枚目のboolean値2", isdisable1, isdisable2);
-      }
-      console.log("ストックナンバー::", stockCardNumber);
-      stockCardNumber.length = 0;
-    }
-  };
-
-  console.log("1,2枚目のboolean値", isdisable1, isdisable2);
-
-  // 1枚目のカード番号を取得しておき、2枚目選択後、不一致であれば そのidのDOMに以下を差し込む。
-  // <div><div class="ReverceImage__Container-yykbpo-0 bSWqJZ"><img src="reverce.jpeg" width="50" height="80" alt="RC"></div></div>
-
   return (
     <>
-      {/* {!(isdisable1 === false || isdisable2 === false) && (
-        <div onClick={(e: any) => handleClick(e)}>
-          <StyledReverceImage />
-        </div>
-      )} */}
       <Container key={num}>
         <span>{mark}</span>
         <div>{num}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,6 @@ import Layout from "../components/Layout";
 import ReverceImage from "../components/ReverceImage";
 import { cardList } from "../utils/card-list";
 
-// TODO random()を発火させてCardコンポーネントを再レンダリングするためのhandlerとそのhandlerを発火するためのButtonを作る。
-
 const Field = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -21,12 +19,11 @@ const stockCardNumber = [];
 const tmpDisableFlugNum = [];
 
 const compareNum = (numArray: number[]) => {
-  // compare cards. if match => match flag is true.
   if (numArray[0] === numArray[1]) {
     alert("マッチ!!!!🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉");
+    tmpDisableFlugNum.length = 0;
     return true;
   }
-  // もし false だった場合（カードがマッチしなかった場合） は isdisable をtrue にする必要がある。
   if (numArray[0] !== numArray[1]) {
     return false;
   }
@@ -40,7 +37,6 @@ const IndexPage = () => {
 
   const handleClick = (num: number, disableFlugNum: number) => {
     if (stockCardNumber.length === 0) {
-      console.log("あああああ");
       if (disableFlugNum === 0) setIsdisable1(false);
       if (disableFlugNum === 1) setIsdisable2(false);
       if (disableFlugNum === 2) setIsdisable3(false);
@@ -49,8 +45,6 @@ const IndexPage = () => {
       stockCardNumber.push(num);
       tmpDisableFlugNum.push(disableFlugNum);
     } else if (stockCardNumber.length === 1) {
-      console.log("ええええ");
-
       if (disableFlugNum === 0) setIsdisable1(false);
       if (disableFlugNum === 1) setIsdisable2(false);
       if (disableFlugNum === 2) setIsdisable3(false);
@@ -62,7 +56,6 @@ const IndexPage = () => {
       if (flag === false) {
         setTimeout(() => {
           // カードを裏側にする。
-          // 選んだ要素の useStateを true にしてあげる。
 
           // 1枚目
           if (tmpDisableFlugNum[0] === 0) setIsdisable1(true);
@@ -75,24 +68,16 @@ const IndexPage = () => {
           if (disableFlugNum === 1) setIsdisable2(true);
           if (disableFlugNum === 2) setIsdisable3(true);
           if (disableFlugNum === 3) setIsdisable4(true);
-
-          setIsdisable1(true);
-          setIsdisable2(true);
         }, 1500);
-        console.log("1,2枚目のboolean値2", isdisable1, isdisable2);
       }
-      console.log("ストックナンバー::", stockCardNumber);
       stockCardNumber.length = 0;
     }
-    console.log("1,2枚目のboolean値1", isdisable1, isdisable2, num);
   };
 
   return (
     <Layout title="👋 Concentration">
       <h1>神経衰弱 at Next.js 👋</h1>
       <Field>
-        {/* {cardList.map((card) => {
-          return ( */}
         <>
           <div>
             <>
@@ -145,8 +130,6 @@ const IndexPage = () => {
             </>
           </div>
         </>
-        {/* ); */}
-        {/* })} */}
       </Field>
     </Layout>
   );
